37bcec956f22b651ea3aa1de146e36022e762190
antonyCandiotti54321 <antonycm54321@gmail.com>
2025-03-07 17:30:34 -0500

    primer commit

---
+++ src/main/java/com/senati/tecnifyproyecto/TecnifyproyectoApplication.java
@@ -0,0 +1,13 @@
+package com.senati.tecnifyproyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecnifyproyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecnifyproyectoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senati/tecnifyproyecto/TecnifyproyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senati.tecnifyproyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecnifyproyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
